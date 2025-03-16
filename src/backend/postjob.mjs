cc2dc8a5247740c4f98add9a44cd5a2e38bed555
Hemakesh Surla <143519486+hsurla@users.noreply.github.com>
2025-03-17 02:06:37 +0530

    con

--- src/backend/postjob.mjs
+++ src/backend/postjob.mjs
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb+srv://demo_user:321@jobportal.r6vad.mongodb.net/', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
